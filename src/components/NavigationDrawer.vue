f0469c9e636a383775b6c45c8c5e57ca8665aeab
Axel <axel.preitit@gmail.com>
2022-10-07 19:10:24 -0300

    Se agregaron Pinia y Vue Router

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,42 +1,46 @@
 <template>
-  <v-navigation-drawer app height="100%" color="blue">
+  <v-navigation-drawer app class="navigation_drawer">
     <v-list-item class="pt-8">
       <v-img src="fiti-logo.png" ></v-img>
     </v-list-item>
     <v-list
+        dark
         dense
+        rounded
         nav
     >
       <v-list-item
           v-for="item in basic_items"
           :key="item.title"
+          :to="item.route"
           link
-          class="white--text"
+          active-class="active_item"
       >
         <v-list-item-icon>
-          <v-icon color="white">{{ item.icon }}</v-icon>
+          <v-icon>{{ item.icon }}</v-icon>
         </v-list-item-icon>
-
         <v-list-item-content>
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
-    <v-divider></v-divider>
+    <v-divider class="divider"></v-divider>
     <v-list
         dense
+        rounded
+        dark
         nav
     >
       <v-list-item
           v-for="item in other_items"
           :key="item.title"
+          :to="item.route"
           link
-          class="white--text"
+          active-class="active_item"
       >
         <v-list-item-icon>
-          <v-icon color="white">{{ item.icon }}</v-icon>
+          <v-icon>{{ item.icon }}</v-icon>
         </v-list-item-icon>
-
         <v-list-item-content>
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item-content>
@@ -52,14 +56,14 @@ export default {
   data () {
     return {
       basic_items: [
-        { title: 'Rutinas creadas', icon: 'fitness_center' },
-        { title: 'Ejercicios creados', icon: 'directions_run' },
-        { title: 'Favoritos', icon:'favorite' },
+        { title: 'Rutinas creadas', icon: 'fitness_center',route:'/about'},
+        { title: 'Ejercicios creados', icon: 'directions_run',route:'/' },
+        { title: 'Favoritos', icon:'favorite',route:'/favorites' },
       ],
       right: null,
       other_items:[
-        {title: 'Configuración',icon:'settings'},
-        {title:'Cerrar sesión',icon:'logout'}
+        {title: 'Configuración',icon:'settings',route:'/login'},
+        {title:'Cerrar sesión',icon:'logout',route:'/about'}
       ]
     }
   },
@@ -67,5 +71,20 @@ export default {
 </script>
 
 <style scoped>
-
+.navigation_drawer{
+  background-color: var(--v-blue-base);
+  height: 100%;
+}
+.divider{
+  color: var(--v-white-base);
+  background-color: white;
+  width: 100%;
+}
+.active_item{
+  color: var(--v-white-base);
+  background-color: var(--v-darkBlue-base);
+}
+.v-list-item--active:before{
+  opacity: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+import router from './router'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  pinia,
+  router,
   render: h => h(App)
 }).$mount('#app')
